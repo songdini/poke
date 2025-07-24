c4b27d10439e00ec2da4d3d938e4e430f52a6fad
songdini <ksssj1371@naver.com>
2025-07-24 14:54:00 +0900

    국립국어원 API 연동6

--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
 import axios from 'axios';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 
 const app = express();
